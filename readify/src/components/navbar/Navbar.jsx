e43c4503bb5289c7f92f2d7aeffb767a72de08ef
gui <149209763+guirdgrs@users.noreply.github.com>
2025-04-24 11:52:24 -0300

    Framer-motion & reusable navbar
    Enhance Navbar with dropdown functionality and animations;
    add reusable NavbarItem component

--- readify/src/components/navbar/Navbar.jsx
+++ readify/src/components/navbar/Navbar.jsx
@@ -1,50 +1,166 @@
-import { useState } from "react";
-import { BookOpenText, User, AlignJustify } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { BookOpenText, User, AlignJustify, Bookmark } from "lucide-react";
+import {motion, AnimatePresence, animate} from "framer-motion";
+import NavbarItem from "./NavbarItem";
 
 function Navbar (){
 
+    // DROPDOWN LEFT MENU
     // State to manage the dropdown menu
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    // Ref to keep track of the dropdown menu element
+    const dropdownRef = useRef(null);
+    // Ref to keep track of the button element
+    const buttonRef = useRef(null);
+
+    // DROPDOWN LOGIN MENU
+    const [dropdownLogin, setDropdownLogin] = useState(false);
+    const loginRef = useRef(null);
+    const loginButtonRef = useRef(null);
 
     // Function to toggle the dropdown menu
     const toggleDropdown = () => {
         setDropdownOpen(!dropdownOpen);
-    }
+    };
+
+    // Function to close the dropdown menu when clicking outside of it
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            // Check if the clicked element is outside the dropdown menu and if the button is clicked
+            if (
+                dropdownRef.current && 
+                !dropdownRef.current.contains(event.target)&& 
+
+                buttonRef.current && 
+                !buttonRef.current.contains(event.target)
+            ){
+                // Close the dropdown menu
+                setDropdownOpen(false);
+            }
+
+            // Same but for the login dropdown
+            if (
+                loginRef.current &&
+                !loginRef.current.contains(event.target) &&
+
+                loginButtonRef.current &&
+                !loginButtonRef.current.contains(event.target)
+            ){
+                setDropdownLogin(false);
+            }
+        };
+
+    // Add event listener to the document to detect clicks outside the dropdown
+     document.addEventListener("mousedown", handleClickOutside);
+     return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
     
     return (
-        <nav 
-        className="bg-violet-500 shadow-md px-6 py-4 flex justify-between sticky top-0 z-50">
+        <motion.nav 
+        className="bg-violet-500 shadow-md px-6 py-4 flex justify-between top-0 z-50 relative"
+        initial={{opacity: 0, y: -20}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 1}}>
 
-            <div className="text-pink-300 hover:text-pink-400 flex items-center gap-3">
-                <a href="#start"><BookOpenText size={40}/></a>
-                <button onClick={toggleDropdown}>
+            <div className="text-pink-300 flex items-center gap-4">
+                {/* Logo */}
+                <motion.a 
+                href="#start"
+                className="hover:text-pink-400"
+                whileHover={{scale: 1.1}}
+                transition={{type: "spring", stiffness: 300}}>
+                    <BookOpenText size={40}/>
+                </motion.a>
+
+                {/* Button to toggle dropdown */}
+                <motion.button 
+                ref={buttonRef}
+                onClick={toggleDropdown}
+                className="hover:text-pink-400"
+                whileHover={{scale: 1.3}}
+                transition={{type: "spring", stiffness: 300}}>
                     <AlignJustify/>
-                </button>
+                </motion.button>
             </div>
 
+            {/* Title */}
+            <motion.p 
+                className="text-bold text-3xl text-pink-300 select-none font-black">
+                    Readify
+                </motion.p>
+
+            {/* Your profile button */}
             <div className="hidden md:flex space-x-6 items-center">
-                <a href="#books" className="text-pink-300 hover:text-pink-400">
-                    Books
-                </a>
-                <a href="#favorites" className="text-pink-300 hover:text-pink-400">
-                    Favorites
-                </a>
-                <a href="#profile" className="text-pink-300 hover:text-pink-400 border-2 rounded-md py-2 px-2">
-                    <User />
-                </a>
+                <motion.button 
+                ref={loginButtonRef}
+                onClick={() => setDropdownLogin(prev => !prev)}
+                className="text-pink-300 hover:text-pink-400 border-2 rounded-md py-2 px-2"
+                whileHover={{scale: 1.1}}
+                transition={{type: "spring", stiffness: 300}}>
+                    <User size={20}/>
+                </motion.button>
             </div>
 
-            {dropdownOpen && (
-                <div className="absolute top-20 left-6 bg-white text-gray-700 shadow-md rounded-md p-4 flex flex-col gap-2 md:hidden z-50">
-                    <a href="#books" className="hover:text-violet-600">
-                        Books</a>
-                    <a href="#favorites" className="hover:text-violet-600">
-                        Favorites</a>
-                    <a href="#profile" className="hover:text-violet-600">
-                        Profile</a>
-                </div>
+            {/* Animated dropown menu */}
+            <AnimatePresence>
+                {dropdownOpen && (
+                    // motion.div is used to animate the dropdown menu
+                    <motion.div
+                        ref={dropdownRef}
+                        initial={{opacity: 0, y: -10}}
+                        animate={{opacity: 1, y: 0}}
+                        exit={{opacity: 0, y: -10}}
+                        transition={{duration: 0.2}}
+                        className="absolute top-20 left-6 bg-violet-300 text-purple-800 rounded-md p-4 px-8 flex flex-col divide-y divide-violet-400 gap-2">
+
+                        {/* Dropdown items */}
+                        <NavbarItem label="Book" href="#books" onClick={() => setDropdownOpen(false)}/>
+
+                        <NavbarItem label="Favorites" href="#favorites" onClick={() => setDropdownOpen(false)}/>
+
+                        <NavbarItem label="About" href="#about" onClick={() => setDropdownOpen(false)}/>
+
+                    </motion.div>
                 )}
-        </nav>
+
+                {/* Dropdown login menu */}
+                {dropdownLogin && (
+                    <motion.div
+                        ref={loginRef}
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        transition={{ duration: 0.2 }}
+                        className="absolute top-20 right-6 bg-violet-300 rounded-md p-4 px-8 w-64 flex flex-col gap-4 z-50">
+
+                        <div className="flex justify-center text-violet-600">
+                            <User size={30} />
+                        </div>
+
+                        <motion.input
+                            type="email"
+                            placeholder="Email"
+                            className="border p-2 rounded-md border-violet-600 outline-pink-600 bg-violet-200"
+                            whileHover={{scale: 1.05}}
+                            transition={{type: "spring", stiffness: 300}}/>
+                        <motion.input
+                            type="password"
+                            placeholder="Password"
+                            className="border p-2 rounded-md border-violet-600 outline-pink-600 bg-violet-200"
+                            whileHover={{scale: 1.05}}
+                            transition={{type: "spring", stiffness: 300}}/>
+                        <motion.button
+                            onClick={() => {
+                            setDropdownLogin(false);}}
+                            className="bg-violet-500 text-white py-2 rounded-md hover:bg-violet-600"
+                            whileHover={{scale: 1.05}}
+                            transition={{type: "spring", stiffness: 300}}>
+                                Login
+                        </motion.button>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </motion.nav>
     );
 };
 
---
+++ readify/src/components/navbar/NavbarItem.jsx
@@ -0,0 +1,18 @@
+import {motion, AnimatePresence} from "framer-motion";
+
+// NavbarItem component to create a reusable navbar item
+// This component is used to create each item in the navbar with hover effects
+function NavbarItem ({label, href = "#", className = "", onClick}){
+    // The props are: label, href, className, and onClick
+    return(
+        <motion.a 
+        href={href}
+        onClick={onClick}
+        className={`hover:text-violet-600 border border-violet-400 rounded-md px-3 py-2 transition-colors ${className}`}
+        whileHover={{scale: 1.15}}
+        transition={{type: "spring", stiffness: 300}}>
+        {label}
+        </motion.a>
+    );
+}
+export default NavbarItem;
