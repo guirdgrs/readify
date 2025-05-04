62be5844458bd2e1bf9a9bebdfd070942d190271
gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-04 12:23:50 -0300

    Visual changes and "about the project" section added on home page.

--- readify/src/components/pages/Home.jsx
+++ readify/src/components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from '../navbar/Navbar.jsx'
 import BookCarousel from '../carousel/BookCarousel.jsx'
+import { motion } from 'framer-motion'; 
 
 function App() {
 
@@ -10,8 +11,33 @@ function App() {
 
   return (
     <div 
-    className="">
+    className="flex flex-col min-h-screen bg-gradient-to-br from-purple-400 via-black-300 to-gray-900 text-white mt-15">
           <Navbar/>
+
+            {/* About Section */}
+            <motion.section
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
+              className="text-center px-4 md:px-20 py-12">
+
+              <h2 
+              className="text-4xl font-bold mb-4 text-pink-400 text-shadow-black text-shadow-2xs">
+                About the project
+              </h2>
+
+              <p 
+              className="text-lg max-w-3xl mx-auto text-gray-300 border border-pink-400 rounded p-4 text-shadow-black text-shadow-2xs">
+                This is a virtual library developed as a personal project to practice and deepen my knowledge in
+                <span 
+                className="text-pink-400 font-semibold"> React</span>, 
+                <span className="text-pink-400 font-semibold"> TailwindCSS </span> 
+                and API consumption, such as the
+                <span className="text-pink-400 font-semibold"> Google Books API</span>. 
+                The goal is to combine learning with a friendly and modern interface.
+              </p>
+            </motion.section>
+
           {/* map is use to go into each item of the array */}
           {genres.map((genres => (
             // for each genre, a BookCarousel component is rendered
