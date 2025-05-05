fd69a5b36ef27bc0fcb23abc7fd45449d5f504cc
gui <149209763+guirdgrs@users.noreply.github.com>
2025-05-05 14:02:55 -0300

    Fixing issues.

--- readify/src/components/pages/About.jsx
+++ readify/src/components/pages/About.jsx
@@ -2,11 +2,13 @@ import { motion } from "framer-motion";
 import { fadeSlideUp } from "../utils/motionConfig.js";
 import Navbar from "../navbar/Navbar.jsx";
 import Footer from "../utils/Footer.jsx";
+import BackButton from "../utils/BackButton.jsx";
 
 function About() {
   return (
     <div>
       <Navbar />
+      <BackButton />
       <motion.div
       className="bg-gradient-to-br text-white from-purple-400 via-black-300 to-gray-900 relative px-6 py-12 flex flex-col items-center mt-20"
       {...fadeSlideUp}>
@@ -28,8 +30,9 @@ function About() {
             className="text-purple-400 font-semibold">
                 React
             </span>. It uses modern web development tools and libraries, including:
+            </p>
             
-            <ul className="list-none list-inside mt-3 text-pink-300 font-bold">
+            <ul className="list-none list-inside mt-3 text-pink-300 font-bold text-xl">
               <li>React + Vite</li>
               <li>TailwindCSS</li>
               <li>React Router</li>
@@ -41,6 +44,8 @@ function About() {
             </ul>
 
             <br />
+          <p 
+          className="text-lg text-whiteleading-relaxed">
             The goal was to build a clean and interactive virtual library interface while learning how to structure components, manage state, and implement animations and routing.
           </p>
         </section>
@@ -57,7 +62,7 @@ function About() {
 
           <p 
           className="text-lg text-white leading-relaxed">
-            I'm a <span className="text-purple-400 font-semibold">Computer Science student</span> trying to pursue a career as a web developer.
+            I'm a <span className="text-purple-400 font-semibold">Computer Science student</span> trying to pursue a career as a developer.
             <br /><br />
             React is just one of the many technologies I'm starting to explore. I'm passionate about learning new tools and improving my skills step by step. This project is part of that journey, and I'm excited to keep building and discovering more in the world of web development.
           </p>
